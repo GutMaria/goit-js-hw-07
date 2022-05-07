1cc64e5d02996b856c19cad8df7d12c89c13547b
GutMaria <gootmaria@gmail.com>
2022-05-08 00:04:45 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+// 1.створення розмітки
+const galleryRef = document.querySelector('.gallery');
+const galleyCards = createGalleryItemCard(galleryItems);
+galleryRef.insertAdjacentHTML('beforeEnd', galleyCards);
+
+function createGalleryItemCard(array) {
+    return array.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+    </a>
+    </div>
+    `
+    }).join('')
+}
+
+
+// 2.event delegation
+
+galleryRef.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+    evt.preventDefault(); 
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return
+    }
+    // 3. модальнe вікно basicLightbox
+    const modalImgToShow = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="800" height="600">`)
+    modalImgToShow.show();
+
+    // document.addEventListener("keydown",
+}
+
+
+
+
